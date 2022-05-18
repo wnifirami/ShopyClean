bad6692c6949923e7406d48a1a3d696e43f92cdd
wnifirami <rami.ounifi@esprit.tn>
2022-05-18 15:57:40 +0100

    first commit project initiation

---
+++ StoreAppSample/App/StoreAppSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppSampleApp.swift
+//  StoreAppSample
+//
+//  Created by Rami Ounifi on 18/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoreAppSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreAppSample
+//
+//  Created by Rami Ounifi on 18/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
